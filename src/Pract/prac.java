34007045e53fd6c6bcbee4809f05c38948e363c3
Aniket Meshram <aniketmeshramofficial@gmail.com>
2023-08-04 00:08:09 +0530

    initial commit

--- src/Pract/prac.java
+++ src/Pract/prac.java
@@ -1,162 +1,70 @@
 package Pract;
 
-import javax.swing.*;
-
 public class prac {
-    static class Node {
-        Node[] children;
-        boolean eow;
-        Node() {
-            children = new Node[26];
-            eow = false;
-        }
-
-    }
-
-    static Node root = new Node();
-    public static void insert(String word) {
-        Node curr = root;
-
-        for (int i = 0; i < word.length(); i++) {
-            int ind = word.charAt(i) - 'a';
-
-            if (curr.children[ind] == null) {
-                curr.children[ind] = new Node();
-            }
-
-            if (i == word.length() - 1) {
-                curr.children[ind].eow = true;
-            }
-
-            curr = curr.children[ind];
-        }
-    }
-
-
-
-    public static boolean search(String key) {
-        Node curr = root;
-
-        for (int i = 0; i < key.length(); i++) {
-            int ind = key.charAt(i) - 'a';
-
-            if (curr.children[ind] == null) {
-                return false;
-            }
-
-            if (i == key.length() - 1 && curr.children[ind].eow == false) {
-                return false;
-            }
-
-            curr = curr.children[ind];
-        }
-        return true;
-    }
-
-
-    public static boolean delete(String key) {
-        Node curr = root;
-
-        for (int i = 0; i < key.length(); i++) {
-            int ind = key.charAt(i) - 'a';
-
-            if (curr.children[ind] == null) {
-                return false;
-            }
-
-            if (i == key.length() - 1 && curr.children[ind].eow == true) {
-                curr.children[ind].eow = false;
-            }
-            curr = curr.children[ind];
-        }
-        return true;
-
-    }
-
-
-
-    public static boolean wordBreak(String key) {
-        if (key.length() == 0) {
-            return true;
-        }
-
-        for (int i = 1; i <= key.length(); i++) {
-            String first = key.substring(0, i);
-            String second = key.substring(i);
-            if (search(first) && wordBreak(second)) {
-                return true;
-            }
-
-        }
-        return false;
-    }
-
-
-    public static int countNode (Node root) {
-        if (root == null) {
-            return 0;
-        }
-
-        int count = 0;
-        for (int i = 0; i < 26; i++) {
-            if (root.children[i] != null) {
-                count += countNode(root.children[i]);
-            }
-        }
-        return count + 1;
-    }
-
-
-
-
-
-    public static boolean startWith(String key) {
-        Node curr = root;
-
-        for (int i = 0; i < key.length(); i++) {
-            int ind = key.charAt(i) - 'a';
-
-            if (curr.children[ind] == null) {
-                return false;
-            }
-
-            curr = curr.children[ind];
-
-        }
-        return true;
-    }
-
-
     public static void main(String[] args) {
-        String[] words = {"banana", "apple", "orange", "watermelen"};
-        for (int i = 0; i < words.length; i++) {
-            insert(words[i]);
+        boolean[][] board = {
+                {false, false, false, false},
+                {false, false, false, false},
+                {false, false, false, false},
+                {false, false, false, false}
+        };
+        Nqueens(board, 0);
+
+    }
+
+    public static void Nqueens(boolean[][] board, int row) {
+
+        if (row == board.length) {
+            display(board);
+            System.out.println();
+            return;
         }
 
-        System.out.println(search("banana"));
-        System.out.println("delete the item: " + delete("orange"));
-        System.out.println("found the element: " + search("orange"));
-//        System.out.println(startWith("man"));
-//
-//        String ans = "aniket";
-//        for (int i = 0; i < ans.length(); i++) {
-//            insert(ans.substring(i));
-//        }
-//
-//        System.out.println(countNode(root));
+        for (int col = 0; col < board.length; col++) {
+            if (isSafe(board, row, col)) {
+                board[row][col] = true;
+                Nqueens(board, row + 1);
+                board[row][col] = false;
+            }
+        }
+    }
 
-//        String[] words = {"i", "like", "samsung", "sung", "ilik"};
-//        for (int i = 0; i < words.length; i++) {
-//            insert(words[i]);
-//        }
-//
-//        String key = "ilikesamsung";
-//        System.out.println(wordBreak(key));
-//
-//        String ans = "aniket";
-//        for (int i = 1; i <= ans.length(); i++) {
-//            System.out.println(ans.substring(0, i));
-//            System.out.println(ans.substring(i));
-//        }
+
+    public static boolean isSafe(boolean[][] board, int row, int col) {
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][col]) {
+                return false;
+            }
+        }
+
+        int maxLeft = Math.min(row, col);
+        for (int i = 1; i <= maxLeft; i++) {
+            if (board[row-i][col-i]) {
+                return false;
+            }
+        }
+
+
+        int maxRight = Math.min(row, board.length - col - 1);
+        for (int i = 1; i <= maxRight; i++) {
+            if (board[row-i][col+i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void display(boolean[][] board) {
+        for (boolean[] row: board ) {
+            for (boolean element: row) {
+                if (element) {
+                    System.out.print("Q ");
+                }
+                else {
+                    System.out.print("X ");
+                }
+            }
+            System.out.println();
+        }
     }
 }

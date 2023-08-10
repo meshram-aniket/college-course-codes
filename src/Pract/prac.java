2401130eb55d2c26cba9e5e8a9a31c4b0ab97023
Aniket Meshram <aniketmeshramofficial@gmail.com>
2023-08-10 14:51:34 +0530

    initial commit

--- src/Pract/prac.java
+++ src/Pract/prac.java
@@ -4,7 +4,11 @@ import java.util.LinkedList;
 
 public class prac {
     public static void main(String[] args) {
-        insertFirst(5);
+
+        //        insertFirst(5);
+//        insertLast(7);
+//        insertLast(6);
+//        insert(1, 10);
         display();
     }
 
@@ -42,12 +46,57 @@ public class prac {
 
     }
 
+
+    public static void insertLast(int val) {
+        Node node = new Node(val);
+        if (head == null) {
+            insertFirst(val);
+            return;
+        }
+        tail.next = node;
+        tail = node;
+        node.next = head;
+    }
+
+
+    public static void insert(int index, int element) {
+
+        if (index == 0) {
+            insertFirst(element);
+            return;
+        }
+
+        if (index == size) {
+            insertLast(element);
+            return;
+        }
+        Node secondLastOfIndex = getIndex(index);
+        Node node = new Node(element, secondLastOfIndex.next);
+        secondLastOfIndex.next = node;
+        node.next = secondLastOfIndex.next.next;
+
+    }
+
+
+    public static Node getIndex(int index) {
+        Node temp = head;
+        for (int i = 1; i <= index - 1; i++) {
+            temp = temp.next;
+        }
+        return temp;
+    }
+
     public static void display() {
         Node temp = head;
+
+        if (head == null) {
+            
+        }
         do {
             System.out.print(temp.val + " -> ");
             temp = temp.next;
         }
         while(temp != head);
+        System.out.println("end");
     }
 }
